aff573a2d95dc2bea14ab1a18f0814aa6e2a0b3b
WSOMNATH <93254275+WSOMNATH@users.noreply.github.com>
2021-11-09 08:05:29 +0530

    qustion5

---
+++ DessertItem.java
@@ -0,0 +1,10 @@
+package oppsassignment;
+
+public abstract class DessertItem {
+    public double tax;
+    public DessertItem (double tax)
+    {
+        this.tax = tax;
+    }
+    public abstract void getCost();
+}
